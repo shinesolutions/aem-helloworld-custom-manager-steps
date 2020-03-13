d0ea260bfcd5fe61cb0d2191599795430ad8a32a
William-Yi-Weng <wingyiwy@hotmail.com>
2020-03-13 15:28:43 +1100

    Update aws_assume_role.rb
    fix whitespace

--- provisioners/puppet/modules/aem_helloworld/lib/puppet/functions/aws_assume_role.rb
+++ provisioners/puppet/modules/aem_helloworld/lib/puppet/functions/aws_assume_role.rb
@@ -28,13 +28,14 @@ Puppet::Functions.create_function(:aws_assume_role) do
   # @param [String] region_name :name of the aws region, for example: ap-southeast-2, us-east-1, etc.
   # @param [Tuple] assume_policy :array of assume aws policy likes: ["s3:ListAllMyBuckets","route53:*"]
   def aws_assume_role(duration_time, session_name, region_name, assume_policy, credential_file)
-    puts "\nGathering credentials..."
+
+    # Gather current assigned IAM role arn from caller identity
+    puts "Gathering credentials..."
     sts_client = Aws::STS::Client.new(region: region_name)
-    resp = sts_client.get_caller_identity()
-    role_name = resp.arn.split('/')[1]
+    sts_resp = sts_client.get_caller_identity()
+    role_name = sts_resp.arn.split('/')[1]
     client = Aws::IAM::Client.new(region: region_name)
     iam_resp = client.get_role(role_name: role_name)
-
     role_arn = iam_resp.role.arn
 
     assume_doc = {
@@ -49,22 +50,21 @@ Puppet::Functions.create_function(:aws_assume_role) do
     }.to_json
 
     # Create a assume role.
-    puts "\nCreating assume role..."
+    puts "Creating assume role..."
     assume_resp = sts_client.assume_role({
                                              duration_seconds: duration_time,
                                              policy: assume_doc,
                                              role_arn: role_arn,
                                              role_session_name: session_name, })
 
-    puts resp.to_h
+    puts assume_resp.assumed_role_user.arn
     access_key_id = assume_resp.credentials.access_key_id
     secret_access_key = assume_resp.credentials.secret_access_key
     session_token = assume_resp.credentials.session_token
     expiration = assume_resp.credentials.expiration
 
     # Record its credentials into credentials file.
-    puts "\nRecord assumed credentials..."
-
+    puts "Record assume credentials..."
     File.open(credential_file, "w") do |f|
       f.write "[assume_role]\naws_access_key_id=#{access_key_id}\naws_secret_access_key=#{secret_access_key}\naws_session_token=#{session_token}\naws_security_token=#{session_token}\nexpiration=#{expiration}\n"
     end
