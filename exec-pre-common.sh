464ef4083d8ed0df88f4861e57666de09ee897c2
William-Yi-Weng <wingyiwy@hotmail.com>
2020-03-13 15:32:59 +1100

    Update exec-pre-common.sh
    add message

--- exec-pre-common.sh
+++ exec-pre-common.sh
@@ -9,6 +9,7 @@ RUN_DIR="provisioners/bash/run-puppet.sh"
 
 if [[ "${AOC_LIBRARY}" == "aem-aws-stack-builder" ]]; then
   if [[ "${AOC_COMMAND}" =~ .*"switch-dns-".* ]]; then
+    echo "[aem-helloworld] Executing aws assume role..."
     source "${RUN_DIR}" "aws_assume_role"
   fi
 fi
