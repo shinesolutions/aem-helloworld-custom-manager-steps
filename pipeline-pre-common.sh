c11cb2243ba47e2cdf85672e6fce6dc4ad64defe
Matthew Dingle <matthew.dingle@shinesolutions.com>
2024-02-20 10:15:55 +1100

    Upgrades packer version

--- pipeline-pre-common.sh
+++ pipeline-pre-common.sh
@@ -2,3 +2,8 @@
 set -o nounset
 
 echo "[aem-helloworld] Executing Custom Manager pipeline-pre-common step..."
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install packer-1.8.6
+
+packer --version
